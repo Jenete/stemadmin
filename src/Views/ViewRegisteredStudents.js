fd3a29e02e2b30791ffdfbaffed59505cd4ba543
Jenete <amahlejenete@gmail.com>
2024-02-12 23:53:11 +0200

    4th commit

--- src/Views/ViewRegisteredStudents.js
+++ src/Views/ViewRegisteredStudents.js
@@ -19,7 +19,11 @@ const ViewRegisteredStudents = ({ students, deleteUser, updateUserDetails }) => 
             [field]: value
         }));
     };
-    
+    const getAmountDue = (subject)=>{
+        if (subject.includes('and'))
+        return 'R250';
+        return '150';
+    }
 
     const FullDetailsModal = () => {
         return (
@@ -29,16 +33,16 @@ const ViewRegisteredStudents = ({ students, deleteUser, updateUserDetails }) => 
                     {selectedStudent && (
                         <div className="student-details">
                             <h2>Full Details</h2>
-                            <p><strong>Name:</strong> <input type="text" value={selectedStudent.learnerName} onChange={(e) => handleInputChange(e, 'learnerName')} /></p>
-                            <p><strong>Grade:</strong> <input type="text" value={selectedStudent.grade} onChange={(e) => handleInputChange(e, 'grade')} /></p>
-                            <p><strong>Subject:</strong> <input type="text" value={selectedStudent.subject} onChange={(e) => handleInputChange(e, 'subject')} /></p>
-                            <p><strong>Cell Number:</strong> <input type="text" value={selectedStudent.cellnumber} onChange={(e) => handleInputChange(e, 'cellnumber')} /></p>
-                            <p><strong>Parent's Cell Number:</strong> <input type="text" value={selectedStudent.parentCell} onChange={(e) => handleInputChange(e, 'parentCell')} /></p>
-                            <p><strong>Parent's Full Name:</strong> <input type="text" value={selectedStudent.parentFullName} onChange={(e) => handleInputChange(e, 'parentFullName')} /></p>
-                            <p><strong>Other Info:</strong> <input type="text" value={selectedStudent.otherInfo} onChange={(e) => handleInputChange(e, 'otherInfo')} /></p>
-                            <p><strong>Start Availability:</strong> <input type="text" value={selectedStudent.startAvailability} onChange={(e) => handleInputChange(e, 'startAvailability')} /></p>
-                            <p><strong>New Date:</strong> <input type="text" value={selectedStudent.newDate} onChange={(e) => handleInputChange(e, 'newDate')} /></p>
-                            <p><strong>Payments:</strong> <input type="text" value={selectedStudent.payments} onChange={(e) => handleInputChange(e, 'payments')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.learnerName} onChange={(e) => handleInputChange(e, 'learnerName')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.grade} onChange={(e) => handleInputChange(e, 'grade')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.subject} onChange={(e) => handleInputChange(e, 'subject')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.cellnumber} onChange={(e) => handleInputChange(e, 'cellnumber')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.parentCell} onChange={(e) => handleInputChange(e, 'parentCell')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.parentFullName} onChange={(e) => handleInputChange(e, 'parentFullName')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.otherInfo} onChange={(e) => handleInputChange(e, 'otherInfo')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.startAvailability} onChange={(e) => handleInputChange(e, 'startAvailability')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.newDate} onChange={(e) => handleInputChange(e, 'newDate')} /></p>
+                            <p><strong></strong> <input type="text" value={selectedStudent.payments} onChange={(e) => handleInputChange(e, 'payments')} /></p>
                         </div>
                     )}
                     <button onClick={() => updateUserDetails(selectedStudent, closeModal)}>Update</button>
@@ -57,17 +61,18 @@ const ViewRegisteredStudents = ({ students, deleteUser, updateUserDetails }) => 
                             <th>Name</th>
                             <th>Grade</th>
                             <th>Subject</th>
-                            <th>Payments</th>
-                            <th>Actions</th>
+                            <th>Exam date</th>
+                            <th>Amount</th>
                         </tr>
                     </thead>
                     <tbody>
                         {students.map((student, index) => (
                             <tr key={index} onClick={() => showUserDetails(student)} className="student-row">
-                                <td>{student.learnerName}</td>
+                                <td>{student.learnerName.split(" ")[0]}</td>
                                 <td>{student.grade}</td>
                                 <td>{student.subject}</td>
-                                <td>{student.payments}</td>
+                                <td>{student.newDate}</td>
+                                <td>{getAmountDue(student.subject)}</td>
                                 <td>
                                     <button onClick={() => deleteUser(student.id)}>Remove</button>
                                 </td>
